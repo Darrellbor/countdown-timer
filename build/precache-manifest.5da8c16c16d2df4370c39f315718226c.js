e3e7f82bc9cbd0dbddbf49fd81ae1f5847759c2a
Darrel Idiagbor <idiagbordarrel@gmail.com>
2020-05-09 05:25:46 +0100

    chore: cleanup for prod

---
+++ build/precache-manifest.5da8c16c16d2df4370c39f315718226c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ffe3dc6d9c36aa0ebaef3dcf5c1dfb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14f5bd101050dbc7291d",
+    "url": "/static/css/2.70ff3058.chunk.css"
+  },
+  {
+    "revision": "2230933551a1af8652ac",
+    "url": "/static/css/main.bf6cb9d1.chunk.css"
+  },
+  {
+    "revision": "14f5bd101050dbc7291d",
+    "url": "/static/js/2.dbc29daa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dbc29daa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2230933551a1af8652ac",
+    "url": "/static/js/main.644e5380.chunk.js"
+  },
+  {
+    "revision": "6ee2ba46a34cd00b0438",
+    "url": "/static/js/runtime-main.f2eb7a5a.js"
+  },
+  {
+    "revision": "28359df250103599e9feb9ce7026dace",
+    "url": "/static/media/body-bg.28359df2.png"
+  },
+  {
+    "revision": "15b0f60b8b295d15244b03d07aedf1a7",
+    "url": "/static/media/countdown-complete.15b0f60b.mp3"
+  }
+]);
